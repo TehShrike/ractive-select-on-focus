d54152f9a431b9450c7ed64c29582adb421582fb
Josh Duff <me@JoshDuff.com>
2016-11-05 18:19:01 -0500

    Why did I ever think I didn't need to listen for focus

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function selectOnFocusDecorator(node) {
 	function onFocus() {
 		node.select()
 	}
-	var events = ['click'] // I don't know if focus would be beneficial?  If so, add it here
+	var events = ['click', 'focus']
 
 	events.forEach(function(key) {
 		node.addEventListener(key, onFocus)
